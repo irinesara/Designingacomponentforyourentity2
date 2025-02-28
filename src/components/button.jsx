d6ff12a85c98886eb80b5cb501ac24ed4f9745d2
irinesara <irine.ixa@gmail.com>
2025-02-28 11:19:38 +0530

    ok

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,14 @@
-// write button card here
+import React from 'react';
+
+const Button = ({ label, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
